96da70491254c902dd0ce845211c79543918492d
Jesus_Desktop <Jesus_Desktop@localhost>
2015-05-10 14:31:59 -0700

    Assets

--- src/game2D/states/GameState.java
+++ src/game2D/states/GameState.java
@@ -1,11 +1,14 @@
 package game2D.states;
 
 import game2D.*;
+import game2D.collisions.*;
 import game2D.projectiles.*;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
 import org.newdawn.slick.openal.*;
 import org.newdawn.slick.state.*;
+import org.newdawn.slick.state.transition.*;
 import org.newdawn.slick.tiled.*;
 
 public abstract class GameState extends BasicGameState {
@@ -14,8 +17,71 @@ public abstract class GameState extends BasicGameState {
 			throws SlickException;
 
 	@Override
-	public abstract void update(GameContainer container, StateBasedGame sbg,
-			int dt) throws SlickException;
+	public void update(GameContainer container, StateBasedGame sbg, int dt)
+			throws SlickException {
+		Vector2f location = new Vector2f();
+		Vector2f pVector = new Vector2f();
+		Input input = container.getInput();
+
+		if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+			sbg.enterState(StateEnum.PAUSE);
+			// container.setPaused(true);
+		} else {
+			if (input.isKeyPressed(Input.KEY_SPACE)) {
+				switchAmmo();
+			}
+			if (input.isKeyDown(Input.KEY_W)) { // Move up
+				if (player.getOriginY() >= 0)
+					pVector.y += -1;
+			}
+			if (input.isKeyDown(Input.KEY_S)) { // Move Down
+				if (player.getEndY() < container.getHeight())
+					pVector.y += 1;
+			}
+			if (input.isKeyDown(Input.KEY_A)) { // Move Left
+				if (player.getOriginX() >= 0)
+					pVector.x += -1;
+			}
+			if (input.isKeyDown(Input.KEY_D)) { // Move Right
+				if (player.getEndX() < container.getWidth())
+					pVector.x += 1;
+			}
+			if (input.isKeyDown(Input.KEY_0)) {
+				Debug();
+			}
+			player.update(pVector, dt);
+			if (player.isDead()) {
+				sbg.enterState(StateEnum.GAME_OVER, new FadeOutTransition(),
+						new FadeInTransition());
+			}
+
+			if (input.isMouseButtonDown(0)) {
+				location.x = player.getCenterX();
+				location.y = player.getCenterY();
+				pVector.x = input.getAbsoluteMouseX() - player.getCenterX();
+				pVector.y = input.getAbsoluteMouseY() - player.getCenterY();
+				pVector = pVector.normalise();
+				if (currentAmmo == AmmoEnum.BULLET) {
+					bulletManager.add(location, pVector);
+				} else if (currentAmmo == AmmoEnum.MISSILE) {
+					missileManager.add(location, pVector);
+				}
+			}
+			missileManager.update(dt);
+			bulletManager.update(dt);
+			enemyManager.update(dt);
+			player.updateAnimation();
+			player.displace(obstacleManager, CollisionEnum.BLOCKING);
+			player.displace(enemyManager, CollisionEnum.DAMAGING);
+			bulletManager.displace(obstacleManager, CollisionEnum.BLOCKING);
+			missileManager.displace(obstacleManager, CollisionEnum.BLOCKING);
+			enemyManager.displace(obstacleManager, CollisionEnum.BLOCKING);
+			enemyManager.displace(bulletManager, CollisionEnum.DAMAGING);
+			enemyManager.displace(missileManager, CollisionEnum.DAMAGING);
+
+			ui.update(player.getHealth());
+		}
+	}
 
 	@Override
 	public abstract void render(GameContainer container, StateBasedGame sbg,
@@ -25,6 +91,13 @@ public abstract class GameState extends BasicGameState {
 	public abstract int getID();
 
 	public abstract void Debug();
+
+	public void switchAmmo() {
+		if (currentAmmo == AmmoEnum.BULLET) {
+			currentAmmo = AmmoEnum.MISSILE;
+		} else
+			currentAmmo = AmmoEnum.BULLET;
+	}
 
 	protected Player player;
 	protected ObstacleManager obstacleManager;
--- src/game2D/states/levels/MapOne.java
+++ src/game2D/states/levels/MapOne.java
@@ -1,7 +1,6 @@
 package game2D.states.levels;
 
 import game2D.*;
-import game2D.collisions.*;
 import game2D.projectiles.*;
 import game2D.states.*;
 import game2D.states.GameState;
@@ -37,7 +36,7 @@ public class MapOne extends GameState {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		gameMap = new TiledMap("maps/level_1.tmx");
+		gameMap = new TiledMap("maps/level_2.tmx");
 		obstacleManager = new ObstacleManager(bounds, 100);
 		missileManager = new MissileManager(bounds, 100);
 		bulletManager = new BulletManager(bounds, 100);
@@ -46,13 +45,10 @@ public class MapOne extends GameState {
 		generateWalls(container);
 		currentAmmo = AmmoEnum.BULLET;
 		try {
+			// backGround = AudioLoader.getAudio("WAV", ResourceLoader
+			// .getResourceAsStream("data/sounds/d_e1m3.wav"));
 			backGround = AudioLoader.getAudio("WAV", ResourceLoader
-					.getResourceAsStream("data/sounds/d_e1m3.wav"));
-			backGround2 = AudioLoader
-					.getAudio(
-							"WAV",
-							ResourceLoader
-									.getResourceAsStream("data/sounds/Crateria_Underground_-_Super_Metroid.wav"));
+					.getResourceAsStream("data/sounds/despair_tower_top.wav"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -74,94 +70,35 @@ public class MapOne extends GameState {
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int dt)
 			throws SlickException {
-		Vector2f location = new Vector2f();
-		Vector2f pVector = new Vector2f();
 		Input input = container.getInput();
-
-		if (input.isKeyPressed(Input.KEY_ESCAPE)) {
-			sbg.enterState(StateEnum.PAUSE);
-			container.setPaused(true);
-		} else {
-			if (input.isKeyPressed(Input.KEY_SPACE)) {
-				switchAmmo();
-			}
-			if (input.isKeyDown(Input.KEY_W)) { // Move up
-				if (player.getOriginY() >= 0)
-					pVector.y += -1;
-			}
-			if (input.isKeyDown(Input.KEY_S)) { // Move Down
-				if (player.getEndY() < container.getHeight())
-					pVector.y += 1;
-			}
-			if (input.isKeyDown(Input.KEY_A)) { // Move Left
-				if (player.getOriginX() >= 0)
-					pVector.x += -1;
-			}
-			if (input.isKeyDown(Input.KEY_D)) { // Move Right
-				if (player.getEndX() < container.getWidth())
-					pVector.x += 1;
-			}
-			if (input.isKeyDown(Input.KEY_0)) {
-				Debug();
-			}
-			player.update(pVector, dt);
-			if (player.isDead()) {
-				sbg.enterState(StateEnum.GAME_OVER, new FadeOutTransition(),
-						new FadeInTransition());
-			}
-			if (input.isKeyPressed(Input.KEY_1)) {
-				currentLevel = 1;
-				sbg.enterState(StateEnum.GAME_LEVEL_ONE,
-						new FadeOutTransition(), new FadeInTransition());
-			}
-			if (input.isKeyPressed(Input.KEY_2)) {
-				currentLevel = 2;
-				sbg.enterState(StateEnum.GAME_LEVEL_TWO,
-						new FadeOutTransition(), new FadeInTransition());
-			}
-			if (input.isKeyPressed(Input.KEY_3)) {
-				currentLevel = 3;
-				sbg.enterState(StateEnum.GAME_LEVEL_THREE,
-						new FadeOutTransition(), new FadeInTransition());
-			}
-			if (input.isKeyPressed(Input.KEY_4)) {
-				if (backGround.isPlaying())
-					backGround.stop();
-				else
-					backGround.playAsMusic(1f, 1f, true);
-			}
-			if (input.isKeyPressed(Input.KEY_5)) {
-				if (backGround2.isPlaying())
-					backGround2.stop();
-				else
-					backGround2.playAsMusic(1f, 1f, true);
-			}
+		super.update(container, sbg, dt);
+		if (input.isKeyPressed(Input.KEY_1)) {
+			currentLevel = 1;
+			sbg.enterState(StateEnum.GAME_LEVEL_ONE, new FadeOutTransition(),
+					new FadeInTransition());
 		}
-
-		if (input.isMouseButtonDown(0)) {
-			location.x = player.getCenterX();
-			location.y = player.getCenterY();
-			pVector.x = input.getAbsoluteMouseX() - player.getCenterX();
-			pVector.y = input.getAbsoluteMouseY() - player.getCenterY();
-			pVector = pVector.normalise();
-			if (currentAmmo == AmmoEnum.BULLET) {
-				bulletManager.add(location, pVector);
-			} else if (currentAmmo == AmmoEnum.MISSILE) {
-				missileManager.add(location, pVector);
-			}
+		if (input.isKeyPressed(Input.KEY_2)) {
+			currentLevel = 2;
+			sbg.enterState(StateEnum.GAME_LEVEL_TWO, new FadeOutTransition(),
+					new FadeInTransition());
 		}
-		missileManager.update(dt);
-		bulletManager.update(dt);
-		enemyManager.update(dt);
-		player.updateAnimation();
-		player.displace(obstacleManager, CollisionEnum.BLOCKING);
-		player.displace(enemyManager, CollisionEnum.DAMAGING);
-		bulletManager.displace(obstacleManager, CollisionEnum.BLOCKING);
-		missileManager.displace(obstacleManager, CollisionEnum.BLOCKING);
-		enemyManager.displace(obstacleManager, CollisionEnum.BLOCKING);
-		enemyManager.displace(bulletManager, CollisionEnum.DAMAGING);
-		enemyManager.displace(missileManager, CollisionEnum.DAMAGING);
-		ui.update(player.getHealth());
+		if (input.isKeyPressed(Input.KEY_3)) {
+			currentLevel = 3;
+			sbg.enterState(StateEnum.GAME_LEVEL_THREE, new FadeOutTransition(),
+					new FadeInTransition());
+		}
+		if (input.isKeyPressed(Input.KEY_4)) {
+			if (backGround.isPlaying())
+				backGround.stop();
+			else
+				backGround.playAsMusic(1f, 1f, true);
+		}
+		if (input.isKeyPressed(Input.KEY_5)) {
+			if (backGround2.isPlaying())
+				backGround2.stop();
+			else
+				backGround2.playAsMusic(1f, 1f, true);
+		}
 	}
 
 	@Override
@@ -191,12 +128,5 @@ public class MapOne extends GameState {
 
 	public void Debug() {
 		return; // Add breakpoint here
-	}
-
-	public void switchAmmo() {
-		if (currentAmmo == AmmoEnum.BULLET) {
-			currentAmmo = AmmoEnum.MISSILE;
-		} else
-			currentAmmo = AmmoEnum.BULLET;
 	}
 }
